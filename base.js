621afc03d9d78a4268624dc7c411d0926fec7b2d
Tiger Oakes <tigeroakes@gmail.com>
2017-01-30 23:12:34 -0800

    Added browser and jest env

--- base.js
+++ base.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
     './rules/best-practices.yaml',
+    './rules/browser.yaml',
     './rules/errors.yaml',
     './rules/es6.yaml',
     './rules/imports.yaml',
+    './rules/jest.yaml',
     './rules/node.yaml',
     './rules/strict.yaml',
     './rules/style.yaml',
